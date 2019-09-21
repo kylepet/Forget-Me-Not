c93dd822dc4cb13c4e2b64bff5cdcc9ee0824ae1
Kyle <kylepetkovic@gmail.com>
2019-09-21 13:34:00 -0700

    Camera viewfinder works 👌👌

--- app/src/main/java/com/shs/forgetmenot/CameraFragment.kt
+++ app/src/main/java/com/shs/forgetmenot/CameraFragment.kt
@@ -6,17 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.otaliastudios.cameraview.CameraListener
 import com.shs.forgetmenot.ui.camera.CameraFragment
+import com.otaliastudios.cameraview.CameraView
+import com.otaliastudios.cameraview.PictureResult
+import com.otaliastudios.cameraview.VideoResult
+
 
 class CameraFragment : Fragment() {
 
     
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.camera_fragment, container, false)
-    
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):View? {
+
+        var view = inflater.inflate(R.layout.camera_fragment, container, false)
+        
+        val camera = view.findViewById<CameraView>(R.id.cameraView)
+        camera.setLifecycleOwner(viewLifecycleOwner);
+
+
+
+        camera.addCameraListener(object : CameraListener() {
+            override fun  onPictureTaken(result: PictureResult) {
+                // A Picture was taken!
+            }
+
+
+            override fun onVideoTaken(result: VideoResult) {
+                // A Video was taken!
+            }
+        })
+        
+        return view
+    }
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        
     }
     
 
 }
+
